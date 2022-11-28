a1d95471975d41d56dbd0d7ddb9d2558599c7915
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2022-11-28 21:47:19 +0100

    flatten json an find all fields

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, jsonify, request
 
+from lib import optimus
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +19,26 @@ def post():
   data = request.get_json()
   return (jsonify({'data': data}))
 
+# endpoint to flatten json
+@app.route('/flatten', methods=['POST'])
+def flatten():
+  print("Flatten request received")
+  data = request.get_json()
+  # flatten json
+  flat_data = optimus.flatten_json(data)  
+  return (jsonify(flat_data))
+
+# endpoint to find all fields in json 
+@app.route('/fields', methods=['POST'])
+def fields():
+  print("Fields request received")
+  data = request.get_json()
+  # flatten json
+  flat_data = optimus.flatten_json(data)  
+  # get all fields witout duplicates
+  fields = list(set(flat_data.keys()))
+  
+  return (jsonify({'fields': fields}))
 
 
 if __name__ == '__main__':
